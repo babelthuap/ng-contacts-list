da03201e39cc8fc666e7b5dce449525ebe2bbe6d
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-22 15:22:34 -0800

    Move all CRUD operations into listCRUD service

--- app.js
+++ app.js
@@ -2,30 +2,37 @@
 
 var app = angular.module('contactsApp', [])
 
-app.controller('listCtrl', function($scope) {
+app.controller('listCtrl', function($scope, listCRUD) {
+  $scope.contacts = listCRUD.contacts;
+  $scope.add = listCRUD.add;
+  $scope.delete = listCRUD.delete;
+  $scope.edit = listCRUD.edit;
+});
+
+app.service('listCRUD', function() {
   // seeded for testing purposes
-  $scope.contacts = [{name: 'Nicholas', email: 'nicholas@gmail.com', phone: '111-111-1111', twitter: '@Babelthuap'},
+  this.contacts = [{name: 'Nicholas', email: 'nicholas@gmail.com', phone: '111-111-1111', twitter: '@Babelthuap'},
                      {name: 'Ben', email: 'dragon@gmail.com', phone: '555-555-5555', twitter: '@dragon'},
                      {name: 'Joan', email: 'neuman@gmail.com', phone: '777-777-7777', twitter: '@neuman'},
                      {name: 'Kaspar', email: 'chun@gmail.com', phone: '123-456-7890', twitter: '@chun'}];
 
-  $scope.add = function() {
-    var newContact = $scope.contact;
-    $scope.contacts.push({
-      name: newContact.name,
-      email: newContact.email,
-      phone: newContact.phone,
-      twitter: newContact.twitter
+  this.add = function(contact) {
+    this.contacts.push({
+      name: contact.name,
+      email: contact.email,
+      phone: contact.phone,
+      twitter: contact.twitter
     });
     return;
   }
 
-  $scope.delete = function(index) {
-    return $scope.contacts.splice(index, 1);
+  this.delete = function(index) {
+    return this.contacts.splice(index, 1);
   }
 
-  $scope.edit = function(index) {
+  this.edit = function(index) {
     console.log('editing', index)
     return;
   }
+
 });
