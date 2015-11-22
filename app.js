38c663ce9ab36b770c595e9e2a737d7a13f969ab
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-22 14:39:32 -0800

    Wire up the delete button

--- app.js
+++ app.js
@@ -3,10 +3,27 @@
 var app = angular.module('contactsApp', [])
 
 app.controller('listCtrl', function($scope) {
-  $scope.contacts = [];
+  // seeded for testing purposes
+  $scope.contacts = [{name: 'Nicholas', email: 'nicholas@gmail.com'},
+                     {name: 'Ben', email: 'dragon@gmail.com'},
+                     {name: 'Joan', email: 'neumann@gmail.com'},
+                     {name: 'Kaspar', email: 'chun@gmail.com'}];
 
   $scope.add = function() {
     var newContact = $scope.contact;
-    $scope.contacts.push({name: newContact.name, email: newContact.email});
+    $scope.contacts.push({
+      name: newContact.name, 
+      email: newContact.email
+    });
+    return;
+  }
+
+  $scope.delete = function(index) {
+    return $scope.contacts.splice(index, 1);
+  }
+
+  $scope.edit = function(index) {
+    console.log('editing', index)
+    return;
   }
 });
