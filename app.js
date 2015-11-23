92050cbbe819dfeaaf958639b619eb3d0aa5500d
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-22 20:39:22 -0800

    Sort by column

--- app.js
+++ app.js
@@ -19,16 +19,19 @@ app.controller('listCtrl', function($scope, listCRUD) {
     $scope.fields.forEach(field => $scope.contact[field] = '');
     return;
   }
+  $scope.sortBy = listCRUD.sortBy;
+  $scope.isSortColumn = listCRUD.isSortColumn;
 });
 
 app.service('listCRUD', function() {
   this.fields = ['name', 'email', 'phone', 'twitter'];
+  var sortColumn = 'name';
 
   // seeded for testing purposes
-  this.contacts = [{name: 'Nicholas', email: 'nicholas@gmail.com', phone: '111-111-1111', twitter: '@Babelthuap'},
+  this.contacts = [{name: 'Nicholas', email: 'nicholas@gmail.com', phone: '123-456-7890', twitter: '@Babelthuap'},
                      {name: 'Ben', email: 'dragon@gmail.com', phone: '555-555-5555', twitter: '@dragon'},
                      {name: 'Joan', email: 'neuman@gmail.com', phone: '777-777-7777', twitter: '@neuman'},
-                     {name: 'Kaspar', email: 'chun@gmail.com', phone: '123-456-7890', twitter: '@chun'}];
+                     {name: 'Kaspar', email: 'chun@gmail.com', phone: '111-111-1111', twitter: '@chun'}];
 
   this.add = function(contact) {
     var newContact = {};
@@ -41,17 +44,64 @@ app.service('listCRUD', function() {
     return this.contacts.splice(index, 1);
   }
 
+  // NOT IMPLEMENTED
   this.edit = function(index) {
     console.log('editing', index)
     return;
   }
 
+  this.sortBy = function(field) {
+    sortColumn = field.toLowerCase();
+    return;
+  }
+
+  this.isSortColumn = function(field) {
+    return field.toLowerCase() === sortColumn;
+  }
+
   this.getContacts = function(query) {
-    if (!query) return this.contacts;
-    // HolyCrapThisIsConcise!
-    return this.contacts.filter(contact => {
-      return this.fields.some(field => contact[field].indexOf(query) !== -1);
-    })
+    if (!query) {
+      return this.contacts.sort((a, b) => a[sortColumn] > b[sortColumn]);
+    } else {
+      // HolyCrapThisIsConcise!
+      var filtered = this.contacts.filter(contact => {
+        return this.fields.some(field => contact[field].indexOf(query) !== -1);
+      })
+      return filtered.sort((a, b) => a[sortColumn] > b[sortColumn]);
+    }
   }
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
