269e1bef9cd8a51514013fa82fa42713a0712352
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-22 21:22:19 -0800

    Sort and filter case-insensitively

--- app.js
+++ app.js
@@ -9,26 +9,31 @@ app.controller('listCtrl', function($scope, listCRUD) {
   $scope.contacts = listCRUD.contacts;
   $scope.edit = listCRUD.edit;
   $scope.getContacts = listCRUD.getContacts;
+  $scope.sortBy = listCRUD.sortBy;
+  $scope.isSortColumn = listCRUD.isSortColumn;
   $scope.delete = index => {
     var deleted = listCRUD.delete(index)[0];
     $scope.fields.forEach(field => $scope.contact[field] = deleted[field]);
     return;
   }
   $scope.add = contact => {
-    listCRUD.add(contact);
-    $scope.fields.forEach(field => $scope.contact[field] = '');
+    if ($scope.fields.every(field => contact[field])) {
+      listCRUD.add(contact);
+      $scope.fields.forEach(field => $scope.contact[field] = '');
+    }
     return;
   }
 });
 
 app.service('listCRUD', function() {
   this.fields = ['name', 'email', 'phone', 'twitter'];
+  var sortColumn = 'name';
 
   // seeded for testing purposes
-  this.contacts = [{name: 'Nicholas', email: 'nicholas@gmail.com', phone: '111-111-1111', twitter: '@Babelthuap'},
-                     {name: 'Ben', email: 'dragon@gmail.com', phone: '555-555-5555', twitter: '@dragon'},
-                     {name: 'Joan', email: 'neuman@gmail.com', phone: '777-777-7777', twitter: '@neuman'},
-                     {name: 'Kaspar', email: 'chun@gmail.com', phone: '123-456-7890', twitter: '@chun'}];
+  this.contacts = [{name: 'Nicholas', email: 'nicholas@gmail.com', phone: '123-456-7890', twitter: '@Babelthuap'},
+                   {name: 'Ben', email: 'dragon@gmail.com', phone: '555-555-5555', twitter: '@dragon'},
+                   {name: 'Joan', email: 'neuman@gmail.com', phone: '777-777-7777', twitter: '@neuman'},
+                   {name: 'Kaspar', email: 'chun@gmail.com', phone: '111-111-1111', twitter: '@chun'}];
 
   this.add = function(contact) {
     var newContact = {};
@@ -46,12 +51,30 @@ app.service('listCRUD', function() {
     return;
   }
 
+  this.sortBy = function(field) {
+    sortColumn = field.toLowerCase();
+    return;
+  }
+
+  this.isSortColumn = field => {
+    return field.toLowerCase() === sortColumn;
+  }
+
+  var sortByColumn = arr => {
+    return arr.sort((a, b) => a[sortColumn].toLowerCase() > b[sortColumn].toLowerCase());
+  }
+
   this.getContacts = function(query) {
-    if (!query) return this.contacts;
-    // HolyCrapThisIsConcise!
-    return this.contacts.filter(contact => {
-      return this.fields.some(field => contact[field].indexOf(query) !== -1);
-    })
+    if (!query) {
+      return sortByColumn(this.contacts);
+    } else {
+      query = query.toLowerCase();
+      // HolyCrapThisIsConcise!
+      var filtered = this.contacts.filter(contact => {
+        return this.fields.some(field => contact[field].toLowerCase().indexOf(query) !== -1);
+      })
+      return sortByColumn(filtered);
+    }
   }
 
 });
